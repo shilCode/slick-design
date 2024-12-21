a584fb9b19b3af056f61498a4fe36627b7f56104
shilcode <2850542@gmail.com>
2024-12-21 21:52:26 +0100

    initial commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+interface Button {
+  title: string;
+  id: string;
+  rightIcon?: string;
+  leftIcon?: string | any;
+  containerClass: string;
+}
+
+const Button = ({ title, id, rightIcon, leftIcon, containerClass }: Button) => {
+  return (
+    <button
+      id={id}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}
+    >
+      {leftIcon}
+      <span className="relative incline-flex overflow-hidden font-general text-xs uppercase">
+        <div>{title}</div>
+      </span>
+      {rightIcon}
+    </button>
+  );
+};
+
+export default Button;
